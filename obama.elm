c9151cd16d670555cda6d786266690d3d63f712e
Michael Dresser <michaelmdresser@gmail.com>
2016-12-10 02:17:36 -0700

    progress on fixing http issues

--- obama.elm
+++ obama.elm
@@ -2,7 +2,7 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Http
-import Json.Decode as Decode
+import Json.Decode exposing (Decoder)
 
 main =
   Html.program
@@ -17,12 +17,12 @@ main =
 
 
 type alias Model =
-  { quote : String }
+    String
 
 
 init : String -> (Model, Cmd Msg)
 init topic =
-  ( Model ""
+  ( ""
   , Cmd.none
   )
 
@@ -31,22 +31,15 @@ init topic =
 -- UPDATE
 
 
-type Msg
-  = NextQuote
-  | NewQuote (Result Http.Error String)
-
+type Msg = FetchContent | ContentReceived
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-    NextQuote ->
-      (model, getNextQuote)
-
-    NewQuote (Ok newUrl) ->
-      (Model model.quote, Cmd.none)
-
-    NewQuote (Err _) ->
-      (model, Cmd.none)
+    FetchContent ->
+        (model, Http.send ContentReceived contentRequest)
+    ContentReceived response -> -- process response
+        (response, Cmd.none)
 
 
 
@@ -57,8 +50,8 @@ view : Model -> Html Msg
 view model =
   div []
     [ h2 [] [text "Obama || Markov"]
-    , button [ onClick NextQuote ] [ text "Next Quote" ]
-    , text model.quote
+    , button [ onClick FetchContent ] [ text "Next Quote" ]
+    , div [] [text model.quote]
     ]
 
 
@@ -73,15 +66,31 @@ subscriptions model =
 -- HTTP
 
 
-getNextQuote : Cmd Msg
-getNextQuote =
-  let
-    addr =
-      "talk-to-obama.herokuapp.com/chat?size=tweet"
-  in
-    Http.send NewQuote (Http.get addr decodeQuoteUrl)
+--getNextQuote : Cmd Msg
+--getNextQuote =
+--  let
+--    addr =
+--      "talk-to-obama.herokuapp.com/chat?size=tweet"
+--  in
+--    Http.send NewQuote (Http.get addr decodeQuoteUrl)
 
 
-decodeQuoteUrl : Decode.Decoder String
-decodeQuoteUrl =
-  Decode.at ["content"] Decode.string
+--decodeQuoteUrl : Decode.Decoder String
+--decodeQuoteUrl =
+--  Decode.at ["content"] Decode.string
+
+
+contentRequest : Http.Request String
+contentRequest =
+  Http.get "talk-to-obama.herokuapp.com/chat?size=tweet" contentDecoder
+
+
+
+
+
+--contentDecoder : Json.Decode.field "content" Json.Decode.string
+--contentDecoder =
+
+contentDecoder : Decoder String
+contentDecoder =
+  Decoder.at ["content"] Decoder.string
